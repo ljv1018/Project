007237294d607568e77c58dc278aaf6ffebfae82
ljv1018 <96320255+ljv1018@users.noreply.github.com>
2022-01-29 17:57:36 -0800

    update windy/humidity

--- index.js
+++ index.js
@@ -76,6 +76,10 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
 }
 
 function showCity(event) {
